c68a3e9bea4bb5a6550e55b2ce2beb59eb48782a
Aeneas Rekkas (arekkas) <aeneas@ory.am>
2016-05-28 21:32:41 +0200

    rand: resolve danger of not reading enough bytes

--- rand/bytes.go
+++ rand/bytes.go
@@ -2,14 +2,15 @@ package rand
 
 import (
 	"crypto/rand"
+	"io"
+	"github.com/go-errors/errors"
 )
 
 // RandomBytes returns n random bytes by reading from crypto/rand.Reader
 func RandomBytes(n int) ([]byte, error) {
 	bytes := make([]byte, n)
-	_, err := rand.Reader.Read(bytes)
-	if err != nil {
-		return []byte{}, err
+	if _, err := io.ReadFull(rand.Reader, bytes); err != nil {
+		return []byte{}, errors.New(err)
 	}
 	return bytes, nil
 }
