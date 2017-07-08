bb74955ead77dbadf2f7b99ec3bff9b27f2a4388
arekkas <aeneas@ory.am>
2017-07-08 19:16:41 +0200

    storage: revoke access tokens when refreshing
    Closes #167

--- handler/oauth2/flow_refresh_storage.go
+++ handler/oauth2/flow_refresh_storage.go
@@ -8,5 +8,8 @@ import (
 
 type RefreshTokenGrantStorage interface {
 	RefreshTokenStorage
+
+	// PersistRefreshTokenGrantSession persists a refresh token grant session. It should delete
+	// old access and refresh tokens, and then persist the new access and refresh signatures.
 	PersistRefreshTokenGrantSession(ctx context.Context, requestRefreshSignature, accessSignature, refreshSignature string, request fosite.Requester) error
 }
