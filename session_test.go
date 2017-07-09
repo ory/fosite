d67d52df200dfc72c9eb79e38ae6e91a1fb701f4
arekkas <aeneas@ory.am>
2017-07-09 17:58:41 +0200

    session: add tests for nil sessions

---
+++ session_test.go
@@ -0,0 +1,14 @@
+package fosite
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSession(t *testing.T) {
+	var s *DefaultSession
+	assert.Empty(t, s.GetSubject())
+	assert.Empty(t, s.GetUsername())
+	assert.Nil(t, s.Clone())
+}
