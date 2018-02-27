77df218b30566ab7cd513b723a7e44f9f6afbe7e
arekkas <aeneas@ory.am>
2018-02-27 11:43:27 +0100

    all: Updates years in license headers

--- handler/oauth2/flow_client_credentials_storage.go
+++ handler/oauth2/flow_client_credentials_storage.go
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2017-2018 Aeneas Rekkas <aeneas+oss@aeneas.io>
+ * Copyright © 2015-2018 Aeneas Rekkas <aeneas+oss@aeneas.io>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
