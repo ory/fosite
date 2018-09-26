d4840aa44cad05d9ad87887b9d86fd4899c77b17
Amir Aslaminejad <aslaminejad@gmail.com>
2018-09-26 11:13:30 -0700

    fosite: use test tables for Hasher unit tests
    Signed-off-by: Amir Aslaminejad <aslaminejad@gmail.com>

--- hash_bcrypt_test.go
+++ hash_bcrypt_test.go
@@ -26,41 +26,88 @@ import (
 
 	"context"
 
-	"github.com/pborman/uuid"
 	"github.com/stretchr/testify/assert"
 )
 
+func TestCompare(t *testing.T) {
+	workfactor := 10
+	hasher := &BCrypt{
+		WorkFactor: workfactor,
+	}
+
+	expectedPassword := "hello world"
+	expectedPasswordHash, err := hasher.Hash(context.TODO(), []byte(expectedPassword))
+	assert.NoError(t, err)
+	assert.NotNil(t, expectedPasswordHash)
+
+	testCases := []struct {
+		testDescription  string
+		providedPassword string
+		shouldError      bool
+	}{
+		{
+			testDescription:  "should not return an error if hash of provided password matches hash of expected password",
+			providedPassword: expectedPassword,
+			shouldError:      false,
+		},
+		{
+			testDescription:  "should return an error if hash of provided password does not match hash of expected password",
+			providedPassword: "some invalid password",
+			shouldError:      true,
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.testDescription, func(t *testing.T) {
+			hash, err := hasher.Hash(context.TODO(), []byte(test.providedPassword))
+			assert.NoError(t, err)
+			assert.NotNil(t, hash)
+
+			err = hasher.Compare(context.TODO(), expectedPasswordHash, []byte(test.providedPassword))
+			if test.shouldError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
+
 func TestHash(t *testing.T) {
-	h := &BCrypt{
-		WorkFactor: 10,
-	}
-	password := []byte("foo")
-	hash, err := h.Hash(context.TODO(), password)
-	assert.NoError(t, err)
-	assert.NotNil(t, hash)
-	assert.NotEqual(t, hash, password)
-}
+	validWorkFactor := 10
+	invalidWorkFactor := 1000 // this is an invalid work factor that will cause the call to Hash to fail!
+	password := []byte("bar")
 
-func TestCompareEquals(t *testing.T) {
-	h := &BCrypt{
-		WorkFactor: 10,
+	testCases := []struct {
+		testDescription string
+		workFactor      int
+		shouldError     bool
+	}{
+		{
+			testDescription: "should succeed if work factor is valid",
+			workFactor:      validWorkFactor,
+			shouldError:     false,
+		},
+		{
+			testDescription: "should fail with error if work factor is invalid",
+			workFactor:      invalidWorkFactor,
+			shouldError:     true,
+		},
 	}
-	password := []byte("foo")
-	hash, err := h.Hash(context.TODO(), password)
-	assert.NoError(t, err)
-	assert.NotNil(t, hash)
-	err = h.Compare(context.TODO(), hash, password)
-	assert.NoError(t, err)
-}
 
-func TestCompareDifferent(t *testing.T) {
-	h := &BCrypt{
-		WorkFactor: 10,
+	for _, test := range testCases {
+		t.Run(test.testDescription, func(t *testing.T) {
+			hasher := &BCrypt{
+				WorkFactor: test.workFactor,
+			}
+
+			_, err := hasher.Hash(context.TODO(), password)
+
+			if test.shouldError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
 	}
-	password := []byte("foo")
-	hash, err := h.Hash(context.TODO(), password)
-	assert.NoError(t, err)
-	assert.NotNil(t, hash)
-	err = h.Compare(context.TODO(), hash, []byte(uuid.NewRandom()))
-	assert.Error(t, err)
 }
