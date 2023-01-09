ad37f5d825500b9b333e71d10045840a0689d100
Romain Caire <romain.caire@shadow.tech>
2023-01-09 12:11:44 +0100

    Use GrantType const string instead of raw string

--- device_authorize_request_handler.go
+++ device_authorize_request_handler.go
@@ -28,7 +28,7 @@ func (f *Fosite) NewDeviceAuthorizeRequest(ctx context.Context, r *http.Request)
 		}
 		request.Client = client
 
-		if !client.GetGrantTypes().Has("urn:ietf:params:oauth:grant-type:device_code") {
+		if !client.GetGrantTypes().Has(string(GrantTypeDeviceCode)) {
 			return nil, errorsx.WithStack(ErrInvalidGrant.WithHint("The requested OAuth 2.0 Client does not have the 'urn:ietf:params:oauth:grant-type:device_code' grant."))
 		}
 	}
