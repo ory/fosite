e2730eecd59689c0f67c5b98f4b73bf0544591f1
hijiki51 <hibiki0719euph@gmail.com>
2023-09-23 13:28:25 +0900

    fix: decode id_token_hint with correct signer
    BREAKING CHANGE: this commit changed OpenIDConnectTokenStrategy interface.
    
    closes #769

--- internal/id_token_strategy.go
+++ internal/id_token_strategy.go
@@ -13,8 +13,8 @@ import (
 	time "time"
 
 	gomock "github.com/golang/mock/gomock"
-
 	fosite "github.com/ory/fosite"
+	jwt "github.com/ory/fosite/token/jwt"
 )
 
 // MockOpenIDConnectTokenStrategy is a mock of OpenIDConnectTokenStrategy interface.
@@ -38,6 +38,21 @@ func NewMockOpenIDConnectTokenStrategy(ctrl *gomock.Controller) *MockOpenIDConne
 // EXPECT returns an object that allows the caller to indicate expected use.
 func (m *MockOpenIDConnectTokenStrategy) EXPECT() *MockOpenIDConnectTokenStrategyMockRecorder {
 	return m.recorder
+}
+
+// DecodeIDToken mocks base method.
+func (m *MockOpenIDConnectTokenStrategy) DecodeIDToken(arg0 context.Context, arg1 fosite.Requester, arg2 string) (*jwt.Token, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "DecodeIDToken", arg0, arg1, arg2)
+	ret0, _ := ret[0].(*jwt.Token)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// DecodeIDToken indicates an expected call of DecodeIDToken.
+func (mr *MockOpenIDConnectTokenStrategyMockRecorder) DecodeIDToken(arg0, arg1, arg2 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "DecodeIDToken", reflect.TypeOf((*MockOpenIDConnectTokenStrategy)(nil).DecodeIDToken), arg0, arg1, arg2)
 }
 
 // GenerateIDToken mocks base method.
