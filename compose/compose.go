f59d68c2a7db34aae4cbc42a83a7cf68be5d5099
Daniel Tam <dtam@cybozu.co.jp>
2023-11-10 17:34:17 +0900

    feat: add support for none response_type

--- compose/compose.go
+++ compose/compose.go
@@ -75,6 +75,7 @@ func ComposeAllEnabled(config *fosite.Config, storage interface{}, key interface
 		OAuth2AuthorizeImplicitFactory,
 		OAuth2ClientCredentialsGrantFactory,
 		OAuth2RefreshTokenGrantFactory,
+		OAuth2NoneResponseTypeFactory,
 		OAuth2ResourceOwnerPasswordCredentialsFactory,
 		RFC7523AssertionGrantFactory,
 
