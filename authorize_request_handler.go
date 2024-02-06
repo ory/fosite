5f324880e3b997f2996a21be8a564808c59af5c7
Nikos <nikos.sklikas@canonical.com>
2024-02-06 10:53:18 +0200

    fix: generalize validateAuthorizeAudience method

--- authorize_request_handler.go
+++ authorize_request_handler.go
@@ -390,7 +390,7 @@ func (f *Fosite) newAuthorizeRequest(ctx context.Context, r *http.Request, isPAR
 		return request, err
 	}
 
-	if err = f.validateAuthorizeAudience(ctx, r, request); err != nil {
+	if err = f.validateAudience(ctx, r, request); err != nil {
 		return request, err
 	}
 
